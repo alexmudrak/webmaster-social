1a898989db9b73c4a1d8655ac09504279bcf4bd2
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2023-12-14 17:27:05 +0200

    feat(fe): mock notification information

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -1,6 +1,12 @@
-import * as React from 'react'
+"use client"
+import NotificationsIcon from '@mui/icons-material/Notifications'
 import AppBar from '@mui/material/AppBar'
+import Badge from '@mui/material/Badge'
 import Box from '@mui/material/Box'
+import Fade from '@mui/material/Fade';
+import IconButton from '@mui/material/IconButton'
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Link from 'next/link'
@@ -20,6 +26,25 @@ export default function NavBar() {
     };
   return (
     <Box sx={{ flexGrow: 1 }}>
+    <Popper
+        sx={{ zIndex: 1200 }}
+        open={open}
+        anchorEl={anchorEl}
+        placement='bottom-end'
+        transition
+      >
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper>
+              <Typography sx={{ p: 2 }}>Notification message 1.</Typography>
+              <Typography sx={{ p: 2 }}>Notification message 2.</Typography>
+              <Typography sx={{ p: 2 }}>Notification message 3.</Typography>
+              <Typography sx={{ p: 2 }}>Notification message 4.</Typography>
+            </Paper>
+
+          </Fade>
+        )}
+      </Popper>
       <AppBar
         position='static'
         sx={{ backgroundColor: 'rgb(243, 244, 246)', color: 'black' }}
@@ -31,12 +56,18 @@ export default function NavBar() {
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             <Link href='/'>Webmaster Social</Link>
           </Typography>
-          <Button color='inherit' href='/dashboard'>
-            Dashboard
-          </Button>
-          <Button color='inherit' href='/settings'>
-            Settings
-          </Button>
+
+          <IconButton
+            size='large'
+            aria-label='show 4 new notifications'
+            color='inherit'
+            onClick={handleClick()}
+          >
+            <Badge badgeContent={4} color='error' >
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+
         </Toolbar>
       </AppBar>
     </Box>
