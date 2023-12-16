c1a585caf36b0e054826a8b9109f874e62ca4c14
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2023-12-16 19:05:52 +0200

    feat(fe): add Run status section to dashboard

---
+++ frontend/src/app/dashboard/RunStatusApp.tsx
@@ -0,0 +1,36 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
+
+import RunStatusAppDetail from './RunStatusAppDetail'
+
+export default function RunStatusApp({ panel, expanded, handleChange }) {
+
+  return (
+    <>
+      <Accordion
+        expanded={expanded === panel}
+        onChange={handleChange(panel)}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls='panel1bh-content'
+          id='panel1bh-header'
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            {'{date}'}
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            {'{project_name} - {publish_name}'}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <RunStatusAppDetail />
+        </AccordionDetails>
+      </Accordion>
+    </>
+  )
+}
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,9 +1,59 @@
+'use client'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Unstable_Grid2'
 import * as React from 'react'
 
+import OverviewApp from './OverviewApp'
+import RunStatusApp from './RunStatusApp'
+
 export default function Page() {
+  const [expanded, setExpanded] = React.useState<string | false>(false)
+
+  const handleChange =
+    (panel: string) => (_: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false)
+    }
   return (
-    <div>
-      Dashboard Page
-    </div>
+    <Container maxWidth='xl' sx={{ marginTop: 2 }}>
+      <Typography
+        sx={{ borderBottom: 1, borderColor: 'divider' }}
+        variant='h4'
+      >
+        Overview
+      </Typography>
+      <Box sx={{ margin: 2 }}>
+        <Grid container spacing={2}>
+          <OverviewApp />
+          <OverviewApp />
+          <OverviewApp />
+        </Grid>
+      </Box>
+
+      <Typography
+        sx={{ borderBottom: 1, borderColor: 'divider' }}
+        variant='h4'
+      >
+        Run status
+      </Typography>
+      <Box sx={{ margin: 2 }}>
+        <RunStatusApp
+          panel='panel_1'
+          expanded={expanded}
+          handleChange={handleChange}
+        />
+        <RunStatusApp
+          panel='panel_2'
+          expanded={expanded}
+          handleChange={handleChange}
+        />
+        <RunStatusApp
+          panel='panel_3'
+          expanded={expanded}
+          handleChange={handleChange}
+        />
+      </Box>
+    </Container>
   )
 }
