6f1ce9e170a5775de5e78e7c69c4da353238ed6a
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2023-12-20 17:28:49 +0200

    feat(be): implement controller for setting v1 api

--- backend/controllers/setting_controller.py
+++ backend/controllers/setting_controller.py
@@ -1,9 +1,9 @@
-from schemas.setting_schema import (
-    SettingCreate,
-    SettingRead,
-    SettingReadWithProject,
-    SettingUpdate,
-)
+from datetime import datetime
+
+from fastapi import HTTPException
+from models.setting import Setting
+from schemas.setting_schema import SettingCreate, SettingUpdate
+from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 
@@ -11,19 +11,70 @@ class SettingController:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_all_objects(self) -> list[SettingRead]:
-        pass
+    async def get_all_objects(self) -> list[Setting]:
+        query = select(Setting)
+        result = await self.session.exec(query)
+        db_objects = result.all()
+        return list(db_objects)
 
-    async def get_object(self, object_id: int) -> SettingReadWithProject:
-        pass
+    async def get_object(self, object_id: int) -> Setting:
+        db_object = await self.session.get(Setting, object_id)
+        if not db_object:
+            raise HTTPException(status_code=404, detail="Setting not found")
+        return db_object
 
-    async def create_object(self, object_data: SettingCreate) -> SettingRead:
-        pass
+    async def create_object(self, object_data: SettingCreate) -> Setting:
+        existing_object = await self.session.exec(
+            select(Setting).where(
+                (Setting.name == object_data.name)
+                & (Setting.project_id == str(object_data.project_id))
+            )
+        )
+        existing_object = existing_object.unique().one_or_none()
+
+        if existing_object:
+            raise HTTPException(
+                status_code=409,
+                detail=(
+                    "A object with the same name and "
+                    "project ID combination already exists."
+                ),
+            )
+
+        db_object = Setting(
+            name=object_data.name,
+            settings=object_data.settings,
+            project_id=object_data.project_id,
+            active=object_data.active,
+        )
+        self.session.add(object)
+        await self.session.commit()
+        await self.session.refresh(object)
+        return db_object
 
     async def update_object(
         self, object_id: int, object_data: SettingUpdate
-    ) -> SettingRead:
-        pass
+    ) -> Setting:
+        db_object = await self.session.get(Setting, object_id)
+
+        if not db_object:
+            raise HTTPException(status_code=404, detail="Setting not found")
+
+        db_object.name = object_data.name
+        db_object.settings = object_data.settings
+        db_object.project_id = object_data.project_id
+        db_object.active = object_data.active
+        db_object.updated = datetime.utcnow()
+
+        await self.session.commit()
+        await self.session.refresh(db_object)
+        return db_object
 
     async def delete_object(self, object_id: int):
-        pass
+        db_object = await self.session.get(Setting, object_id)
+        if not db_object:
+            raise HTTPException(status_code=404, detail="Setting not found")
+
+        await self.session.delete(db_object)
+        await self.session.commit()
+        return {"message": f"Setting`{object_id}` was deleted"}
