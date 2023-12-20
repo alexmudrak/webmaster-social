dd1bd284eff0ca885ab6ca62cf488914193d33bf
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2023-12-20 17:02:00 +0200

    feat(be): add base api and controller for setting

--- backend/api/v1_settings.py
+++ backend/api/v1_settings.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from controllers.setting_controller import SettingController
+from core.database import get_session
+from fastapi import APIRouter, Depends, status
+from schemas.setting_schema import (
+    SettingCreate,
+    SettingRead,
+    SettingReadWithProject,
+    SettingUpdate,
+)
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 router = APIRouter(
     prefix="/settings",
@@ -8,7 +17,59 @@ router = APIRouter(
 
 @router.get(
     "/",
-    summary="Get all settings from app.",
+    summary="Get all settings",
+    response_model=list[SettingRead],
 )
-async def get_all_objects():
-    return "Hello Settings!"
+async def get_all_settings(
+    session: AsyncSession = Depends(get_session),
+):
+    return SettingController(session).get_all_objects()
+
+
+@router.get(
+    "/{setting_id}",
+    summary="Get setting by ID",
+    response_model=SettingReadWithProject,
+)
+async def get_object_by_id(
+    object_id: int,
+    session: AsyncSession = Depends(get_session),
+):
+    return SettingController(session).get_object(object_id)
+
+
+@router.post(
+    "/",
+    summary="Create new setting",
+    response_model=SettingRead,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_object(
+    object_data: SettingCreate,
+    session: AsyncSession = Depends(get_session),
+):
+    return SettingController(session).create_object(object_data)
+
+
+@router.patch(
+    "/{project_id}",
+    summary="Update setting by ID",
+    response_model=SettingRead,
+)
+async def update_object(
+    object_id: int,
+    object_data: SettingUpdate,
+    session: AsyncSession = Depends(get_session),
+):
+    return SettingController(session).update_object(object_id, object_data)
+
+
+@router.delete(
+    "/{setting_id}",
+    summary="Delete setting by ID",
+)
+async def delete_object(
+    object_id: int,
+    session: AsyncSession = Depends(get_session),
+) -> dict[str, str]:
+    return SettingController(session).delete_object(object_id)
--- backend/controllers/setting_controller.py
+++ backend/controllers/setting_controller.py
@@ -1,8 +1,9 @@
-from datetime import datetime
-
-from fastapi import HTTPException
-from models.project import Project, ProjectCreate, ProjectUpdate
-from sqlmodel import select
+from schemas.setting_schema import (
+    SettingCreate,
+    SettingRead,
+    SettingReadWithProject,
+    SettingUpdate,
+)
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 
@@ -10,74 +11,19 @@ class SettingController:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_all_objects(self) -> list[Project]:
-        query = select(Project)
-        result = await self.session.exec(query)
-        projects = result.all()
-        return list(projects)
+    async def get_all_objects(self) -> list[SettingRead]:
+        pass
 
-    async def get_object(self, object_id: int) -> Project:
-        project = await self.session.get(Project, object_id)
-        if not project:
-            raise HTTPException(status_code=404, detail="Project not found")
-        return project
+    async def get_object(self, object_id: int) -> SettingReadWithProject:
+        pass
 
-    async def create_object(self, object_data: ProjectCreate) -> Project:
-        if not object_data.url:
-            raise HTTPException(status_code=400, detail="Invalid URL provided")
-
-        existing_project = await self.session.exec(
-            select(Project).where(
-                (Project.name == object_data.name)
-                & (Project.url == str(object_data.url))
-            )
-        )
-        existing_project = existing_project.one_or_none()
-
-        if existing_project:
-            raise HTTPException(
-                status_code=409,
-                detail=(
-                    "A project with the same name and "
-                    "URL combination already exists."
-                ),
-            )
-
-        project = Project(
-            name=object_data.name,
-            url=str(object_data.url),
-            active=object_data.active,
-        )
-        self.session.add(project)
-        await self.session.commit()
-        await self.session.refresh(project)
-        return project
+    async def create_object(self, object_data: SettingCreate) -> SettingRead:
+        pass
 
     async def update_object(
-        self, object_id: int, object_data: ProjectUpdate
-    ) -> Project:
-        if not object_data.url:
-            raise HTTPException(status_code=400, detail="Invalid URL provided")
+        self, object_id: int, object_data: SettingUpdate
+    ) -> SettingRead:
+        pass
 
-        project = await self.session.get(Project, object_id)
-
-        if not project:
-            raise HTTPException(status_code=404, detail="Project not found")
-
-        project.name = object_data.name
-        project.url = str(object_data.url)
-        project.active = object_data.active
-        project.updated = datetime.utcnow()
-
-        await self.session.commit()
-        await self.session.refresh(project)
-        return project
-
-    async def delete_object(self, object_id: int) -> dict[str, str]:
-        project = await self.session.get(Project, object_id)
-        if not project:
-            raise HTTPException(status_code=404, detail="Project not found")
-
-        await self.session.delete(project)
-        await self.session.commit()
-        return {"message": f"Project `{object_id}` was deleted"}
+    async def delete_object(self, object_id: int):
+        pass
