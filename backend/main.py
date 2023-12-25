d3f29e75032d4f3e96217c44409332646bacd829
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2023-12-25 13:19:39 +0200

    chore: delete unused code

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,6 @@ from api.v1_projects import router as projects_router
 from api.v1_settings import router as settings_router
 from api.v1_tasks import router as tasks_router
 from fastapi import FastAPI
-from models.project import Project
-from models.setting import Setting
-
-Setting.model_rebuild()
-Project.model_rebuild()
 
 app = FastAPI()
 
