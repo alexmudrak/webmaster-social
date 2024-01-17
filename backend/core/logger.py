1c52075e47b9801d95197e7b636498dc2642b2b8
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2024-01-17 12:24:39 +0200

    feat(be): implement logger handler for writing to LogEntry model in database

--- backend/core/logger.py
+++ backend/core/logger.py
@@ -1,8 +1,27 @@
+import asyncio
 import logging
 
+from core.database import get_session_context
 from core.settings import Settings
+from models.log_entry import LogEntry
 
 SETTINGS = Settings()
+
+
+class DatabaseHandler(logging.Handler):
+    def emit(self, record):
+        asyncio.create_task(self.async_emit(record))
+
+    async def async_emit(self, record):
+        log_entry = LogEntry(
+            level=record.levelname.replace(":", ""),
+            logger_name=record.name,
+            message=self.format(record),
+        )
+
+        async with get_session_context() as session:
+            session.add(log_entry)
+            await session.commit()
 
 
 class CustomFormatter(logging.Formatter):
@@ -12,20 +31,27 @@ class CustomFormatter(logging.Formatter):
 
 
 def get_logger(
-    logger_name,
-    log_level=SETTINGS.LOG_LEVEL,
+    logger_name: str,
+    log_level: int | str | None = None,
 ):
     logger = logging.getLogger(logger_name)
-    logger.setLevel(log_level)
 
-    formatter = CustomFormatter(
-        "%(levelname)-9s %(asctime)s - [%(name)s] - %(message)s"
-    )
+    if not logger.handlers:
+        logger.setLevel(log_level or SETTINGS.LOG_LEVEL)
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(log_level)
-    console_handler.setFormatter(formatter)
+        formatter = CustomFormatter(
+            "%(levelname)-9s %(asctime)s - [%(name)s] - %(message)s"
+        )
+        db_formatter = logging.Formatter("%(message)s")
 
-    logger.addHandler(console_handler)
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+        db_handler = DatabaseHandler()
+        db_handler.setFormatter(db_formatter)
+        logger.addHandler(db_handler)
+
+        logger.propagate = False
 
     return logger
