213ca828777e8caeb0dcfb204a824145ebbed047
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2024-01-18 11:36:52 +0200

    feat(be): implement util function for strings - truncate_text

--- backend/utils/string_handler.py
+++ backend/utils/string_handler.py
@@ -1,5 +1,34 @@
+import textwrap
+
+
 def escape_markdown(text: str) -> str:
+    """
+    Markdown special characters like *, _, and [] will be escaped with a
+    backslash so that they are treated as literal characters when the text
+    is rendered in markdown.
+    """
     escape_chars = "_*[]()~`>#+-=|{}.!"
     return "".join(
         f"\\{char}" if char in escape_chars else char for char in text
+    )
+
+
+def truncate_text(text: str, max_length: int, placeholder: str = "...") -> str:
+    """
+    Truncate a message to a maximum length, ensuring that the truncation
+    occurs at a word boundary and ends with an ellipsis if truncated.
+    """
+    check_length = max_length - len(placeholder)
+
+    if check_length < 0:
+        return textwrap.shorten(
+            text,
+            width=max_length,
+            placeholder="",
+        )
+
+    return textwrap.shorten(
+        text,
+        width=max_length,
+        placeholder=placeholder,
     )
